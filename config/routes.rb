98305ffb3a95478c93d213a6bcd739150ea83588
Yason Mogutin <yason4ik87@gmail.com>
2016-03-22 14:51:44 +0200

    root_path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ DemoApp::Application.routes.draw do
 
   resources :users
 
-  root 'microposts#index'
+  root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
